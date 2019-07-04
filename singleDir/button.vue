39d73e0fc976a0a22e76008ee765401ecfd411cf
BLZbanme <swordcome@gmail.com>
2019-07-05 04:19:13 +0800

    finish app.vue

---
+++ singleDir/button.vue
@@ -0,0 +1,47 @@
+<template>
+    <button
+    @click="handleClick" :style="styles">
+        <slot></slot>
+    </button>
+</template>
+<script>
+    export default {
+        props: {
+            title: {
+                type: String
+            }
+        },
+        computed: {
+            styles() {
+                return {
+                    background: this.color
+                }
+            }
+        },
+        methods: {
+            handleClick(e) {
+                this.$emit("click", e);
+            }
+        }
+    }
+</script>
+<style>
+    button {
+        border: 0;
+        outline: none;
+        color: #fff;
+        padding: 4px 8px;
+    }
+    button:active {
+        position: relative;
+        top: 1px;
+        left: 1px;
+    }
+</style>
+
+<style>
+    h1 a {
+        color: #3399ff;
+        font-size: 24px;
+    }
+</style>
